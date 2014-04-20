d0daca49e75153bac96ca31fc1a68e2764edd697
aore <aore@krellinst.org>
2014-04-20 13:15:48 -0500

    Added echo test

--- control/tests/sentinel_basics.py
+++ control/tests/sentinel_basics.py
@@ -8,6 +8,7 @@ sen.ping()
 
 sen.write(b'\x01\x02\x06foobar\x06\x00\x02')  # prompts an echo
 sen.write(b'\x01\x02\x0Cfoobarfoobar\x0C\x00\x02')  # prompts an echo
+sen.write(b'\x01\x02\x0a' + b'0123456789' + b'\x0a\x01' + b'\x01A\x01\x00\x02') # prompts an echo with two frames
 
 sen.echo("")
 sen.echo("short_string")
