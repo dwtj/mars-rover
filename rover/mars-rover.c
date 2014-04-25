29fb217d5cca60222c44943a0c58224e1bf885f3
David Johnston <dj@djohnston.me>
2014-04-25 13:23:45 -0500

    Made song test code in `main()` into its own mode to be selected.

--- rover/mars-rover.c
+++ rover/mars-rover.c
@@ -10,26 +10,31 @@
 
 int main()
 {
-		char song[] = {96, 96, 96, 96, 92, 94, 96, 94, 96};
+    init_push_buttons();
+    lcd_init();
+
+    switch (wait_button("Select Mode")) {
+    case 1:
+	    control_mode();
+        break;
+
+    case 2:
+        send_mesg_test_mode();
+        break;
+
+    case 3:
+	    objects_lab();
+        break;
+
+    case 4:
+        char song[] = {96, 96, 96, 96, 92, 94, 96, 94, 96};
 		char duration[] = {8, 8, 8, 8, 12, 12, 8, 8, 8}; //These probably need to be edited.
 		oi_load_song(0,9, song[0], duration[0]);//??
 		oi_play_song(0);
-		
-//     init_push_buttons();
-//     lcd_init();
-// 
-//     switch (wait_button("Select Mode")) {
-//     case 1:
-// 	    control_mode();
-//         break;
-//     case 2:
-//         send_mesg_test_mode();
-//         break;
-//     case 3:
-// 	    objects_lab();
-//         break;
-//     default:
-//         lcd_puts("Invalid selection.");
-//         break;
-//     }
+        break;
+
+    default:
+        lcd_puts("Invalid selection.");
+        break;
+    }
 }
